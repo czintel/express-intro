054f8101b6c37103e4afc8262e104ffe562b8300
Christian Zintel <c.zintel@yahoo.de>
2021-05-28 11:00:50 +0200

    Added router backend index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.get("/", function (req, res) {
-  res.send("hello");
+app.use(routes);
+
+app.use(function (req, res) {
+  res.status.apply(404).send("Not found");
+});
+
+app.use(function (error, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
 
 app.listen(3000, function () {
